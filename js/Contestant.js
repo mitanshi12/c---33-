16ad1fdc879bdfed9b991b7f2a4057ddc49b857e
mitanshi raikwar <88498181+mitanshi12@users.noreply.github.com>
2021-11-02 15:09:46 +0530

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,27 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+   //get the Count from the database
+  }
+
+  updateCount(count){
+   //update the database
+  }
+
+ /*  update(){
+    var contestantIndex = "contestants/contestant" + this.index;
+    database.ref(contestantIndex).set({
+      name:this.name,
+      answer:this.answer
+    });
+  } */
+
+
+ 
+}
